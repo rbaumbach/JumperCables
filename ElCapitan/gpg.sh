9b074cbe835bccca4ee566191c7816816601c097
Ryan <github@ryan.codes>
2016-04-17 14:45:46 -0700

    Update gpg.sh to copy a local copy of .gnupg to home directory if present

--- ElCapitan/gpg.sh
+++ ElCapitan/gpg.sh
@@ -6,9 +6,30 @@ brew cask install gpg2
 
 echo 'use-standard-socket' >> ~/.gnupg/gpg-agent.conf
 
-ececho '# gpg2' >> ~/.bash_profile
+echo '# gpg2' >> ~/.bash_profile
 echo '' >> ~/.bash_profile
 echo 'export GPG_TTY=$(tty)' >> ~/.bash_profile
 echo '' >> ~/.bash_profile
 
-gpg-agent --daemon
+NEW_GPG_DIR="$(dirname "$0")/.gnupg"
+CURRENT_GPG_DIR="~/.gnupg"
+CURRENT_GPG_DIR_BAK="~/.gnupg_bak"
+HOME_DIR="~/"
+
+if [ -d "$NEW_GPG_DIR" ]
+then
+    if [ -d "$CURRENT_GPG_DIR" ]
+        echo
+        echo "Found existing $CURRENT_GPG_DIR, renaming to $CURRENT_GPG_DIR_BAK"
+        mv ${CURRENT_GPG_DIR} ${CURRENT_GPG_DIR_BAK}
+    then
+    fi
+    
+    echo
+    echo "Copying over $NEW_GPG_DIR to $CURRENT_GPG_DIR"
+    cp -avR #{NEW_GPG_DIR} #{HOME_DIR}
+    
+    echo
+    echo "Staring gpg-agent"
+    gpg-agent --daemon    
+fi
--- ElCapitan/xcode-license.sh
+++ ElCapitan/xcode-license.sh
@@ -2,5 +2,4 @@
 
 echo
 echo "Accepting Xcode Software License Agreement..."
-
 sudo xcodebuild -license accept
