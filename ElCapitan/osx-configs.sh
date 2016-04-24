05b4a8433343f8dd82687aa175db40f6be0c8063
Ryan <github@ryan.codes>
2016-04-24 10:12:44 -0700

    Add more osx-configs

--- ElCapitan/osx-configs.sh
+++ ElCapitan/osx-configs.sh
@@ -4,25 +4,58 @@ echo
 echo "Customizing OS X configurations"
 
 echo
-echo "Updating Finder configurations"
+echo "Updating Dock configurations"
 
 echo
-echo "Hiding the Dock"
-defaults write com.apple.dock autohide -bool true
+echo "Hide the Dock"
+defaults write com.apple.dock autohide -bool YES
+
+echo
+echo "Update Dock minimize effect to 'Scale'"
+defaults write com.apple.dock mineffect -string scale
+
+echo
+echo "Reloading Dock"
 killall Dock
 
 echo
-echo "Displaying full path in title bar"
-defaults write com.apple.finder '_FXShowPosixPathInTitle' -bool true
+echo "Updating Finder configurations"
+
+echo
+echo "Display full path in title bar"
+defaults write com.apple.finder '_FXShowPosixPathInTitle' -bool YES
+
+echo
+echo "Show hard drives on the desktop"
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool YES
+
+echo
+echo "Show mounted servers on the desktop"
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool YES
+
+echo
+echo "Reloading Finder"
 killall Finder
+
+echo
+echo "Updating System Preferences"
+
+echo
+echo "Disable Gatekeeper auto renewal"
+sudo defaults write /Library/Preferences/com.apple.security GKAutoRearm -bool NO
 
 echo
 echo "Updating Safari Configurations"
 
 echo
-echo "Disabling PDF opening..."
+echo "Disable auto PDF opening..."
 defaults write com.apple.Safari WebKitOmitPDFSupport -bool YES
 
 echo
-echo "Disabling auto open safe downloading"
+echo "Disable auto open safe filedownloading"
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool NO
+
+echo
+echo "Disable Photos from opening automatically"
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
