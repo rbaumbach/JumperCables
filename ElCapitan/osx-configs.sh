0e8b1694a60a13c5534283be2760e4e401c97d39
Ryan <github@ryan.codes>
2016-04-24 14:03:16 -0700

    Addingmore osx-configs

--- ElCapitan/osx-configs.sh
+++ ElCapitan/osx-configs.sh
@@ -35,6 +35,14 @@ echo "Show mounted servers on the desktop"
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool YES
 
 echo
+echo "Show file extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool YES
+
+echo
+echo "Show status bar"
+defaults write com.apple.finder ShowStatusBar -bool YES
+
+echo
 echo "Reloading Finder"
 killall Finder
 
@@ -59,3 +67,18 @@ defaults write com.apple.Safari AutoOpenSafeDownloads -bool NO
 echo
 echo "Disable Photos from opening automatically"
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
+
+echo
+echo "Updating other configurations"
+
+echo
+echo "Disable 'smart' quotes"
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool NO
+
+echo
+echo "Disable 'smarl' dashes"
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool NO
+
+echo
+echo "Disable .DS_File writes on network stores"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool YES
