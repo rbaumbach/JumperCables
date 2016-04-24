c9cb7355fa2b728704b1554ae16f291919d003aa
Ryan <github@ryan.codes>
2016-04-24 14:05:14 -0700

    Update mac-app-store.sh

--- ElCapitan/mac-app-store.sh
+++ ElCapitan/mac-app-store.sh
@@ -3,8 +3,8 @@
 echo
 echo "Installing Mac App Store apps"
 brew install argon/mas/mas
+mas install 409907375 # Apple Remote Desktop
+mas install 409183694 # Keynote
+mas install 715768417 # Microsoft Remote Deskop
 mas install 409203825 # Numbers
 mas install 409201541 # Pages
-mas install 409183694 # Keynote
-mas install 409907375 # Apple Remote Desktop
-mas install 715768417 # Microsoft Remote Deskop
