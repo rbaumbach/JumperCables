5bc9d1d06d01f3baabcda9cfda708e4fabdc70c3
rbaumbach <github@ryan.codes>
2016-09-25 21:03:04 -0700

    Disable Homebrew tracking, add Homebrew Github API token scaffolding

--- ElCapitan/homebrew.sh
+++ ElCapitan/homebrew.sh
@@ -6,7 +6,7 @@ if hash brew 2>/dev/null; then
 else
     echo "Installing Homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  
+
     echo
     echo "Ensuring your Homebrew directory is writable..."
     chown -R $(whoami) /usr/local/bin
@@ -20,7 +20,7 @@ echo
 echo "Upgrading existing brews..."
 brew upgrade
 
-echo 
+echo
 echo "Ensuring you have a healthy Homebrew environment..."
 brew doctor
 
@@ -28,3 +28,14 @@ echo
 echo "Cleaning up your Homebrew installation..."
 brew cleanup
 
+echo
+echo "Disabling Homebrew Tracking"
+echo '# Homebrew' >> ~/.bash_profile
+echo '' >> ~/.bash_profile
+echo 'export GITAWAREPROMPT=~/.bash/git-aware-prompt' >> ~/.bash_profile
+echo '' >> ~/.bash_profile
+source ~/.bash_profile
+
+echo "Adding Homebrew Github API token scaffolding."
+echo '# export HOMEBREW_GITHUB_API_TOKEN=<Add your token from Github here>' >> ~/.bash_profile
+echo '' >> ~/.bash_profile
--- ElCapitan/the-end.sh
+++ ElCapitan/the-end.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo
+echo "Be sure to add your Github Homebrew API token to ~/.bash_profile"
+
+echo
 echo "FIN!"
