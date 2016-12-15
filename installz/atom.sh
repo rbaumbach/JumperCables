29b409957535fdbd5f69f80cbc2d78e7575a823b
Ryan <github@ryan.codes>
2016-12-14 21:47:24 -0800

    Add some atom packages

--- installz/atom.sh
+++ installz/atom.sh
@@ -8,5 +8,17 @@ brew cask install atom
 echo
 echo "Installing atom packages"
 
+apm install atom-bootstrap3
 apm install atom-html-preview
-apm install atom-bootstrap3 
+apm install expose
+apm install file-icons
+apm install pigments
+apm install highlight-selected
+apm install rest-client
+apm install todo-show
+
+apm install linter
+apm install linter-shellcheck
+apm install linter-bootlint
+
+apm install xkcd-comics
