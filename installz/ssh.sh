aee97eaba60a718d81a1a579a6dfb686e5e317aa
rbaumbach <github@ryan.codes>
2017-01-11 21:12:54 -0800

    Fix config.ssh and remove some comments

--- installz/ssh.sh
+++ installz/ssh.sh
@@ -21,17 +21,6 @@ else
   ssh-keygen -t rsa -b 4096 -C $SSH_KEY_EMAIL -f $SSH_KEY_FILE
 fi
 
-
-
-# SSH_EMAIL="baumbach@ryan.codes"
-# DEFAULT_KEY_FILE=${HOME}/.ssh/id_rsa
-# LOCAL_USER_SSH_DIR=${HOME}/.ssh
-# LOCAL_USER_SSH_DIR_BAK=${HOME}/.ssh_bak
-#
-# echo
-# echo "Generating default ssh key"
-# ssh-keygen -t rsa -b 4096 -C $SSH_EMAIL -f $DEFAULT_KEY_FILE
-#
 # echo
 # echo "Starting ssh-agent..."
 # eval "$(ssh-agent -s)"
